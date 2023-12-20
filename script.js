1e1b1c966ebfe57c6322ce507baf150b7ed24be7
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-21 01:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+// 	s.trim();
+// var a=0;
+//   for (let index = 0; index < s.length; index++) {
+//   	 if(s[index]!==" "){
+// 		 a=index;
+//          break;
+// 	 }
+//   }
+// 	var b=0;
+// 	for (let i = 0; i <s.length; i++) {
+// 		if(s[i]==" "){
+// 			b=i;
+// 			break;
+// 		}
+// 	}
+//     // return b;
+// 	// if(b>0)
+// 	return s.substring(a,b);
+// 	return "";
+	// let a=s.trim;
+	// let b=a.split(" ");
+	// return b;
+	if (!str.trim()) {
+        return '';
+    }
+
+    // Split the string by spaces
+    const words = str.trim().split(' ');
+
+    // Return the first word
+    return words[0];
 }
 
 // Do not change the code below
+const str = prompt("Enter String:");
+alert(firstWord(str));
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
